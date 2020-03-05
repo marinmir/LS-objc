2fd06e07519bb72ad43bb392699254a328846bdc
Miroshnichenko Marina <marinamirosh99@gmail.com>
2020-03-06 00:20:57 +0300

    Initial Commit

---
+++ LS-objc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LS-objc
+//
+//  Created by Мирошниченко Марина on 06.03.2020.
+//  Copyright © 2020 Мирошниченко Марина. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
